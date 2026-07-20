39bb8cc05e0f1203a1c3036e8808c515a7c23d21
Akshit Bhardwaj <akshitbhardwaj257448@gmail.com>
2026-07-20 22:49:25 +0530

    feat: add Alumni DocType with contact and academic details

---
+++ alumni_nit_srinagar/alumni_nit_srinagar/doctype/alumni/alumni.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Akshit Bhardwaj and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Alumni", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ alumni_nit_srinagar/alumni_nit_srinagar/doctype/alumni/alumni.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2026, Akshit Bhardwaj and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Alumni(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		batchyear: DF.Data
+		branchdepartment: DF.Literal["Computer Science & Engineering (CSE)", "Information Technology (IT)", "Electrical Engineering", "Electronics & Communication Engineering (ECE)", "Mechanical Engineering", "Civil Engineering", "Chemical Engineering", "Metallurgical & Materials Engineering", "Mathematics", "Physics", "Chemistry", "Humanities, Social Sciences & Management (HSS&M)"]
+		email_address: DF.Data
+		full_name: DF.Data
+		phone_number: DF.Phone | None
+	# end: auto-generated types
+
+	_DOCTYPE_NAME = "Alumni"
